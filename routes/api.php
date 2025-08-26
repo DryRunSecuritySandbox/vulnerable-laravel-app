fbbf84e781046c4ef3cd0253821d4dda97a04057
Ken Johnson <cktricky@gmail.com>
2025-08-26 13:42:38 -0400

    Add route to get upload details by ID

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,6 @@
 Route::get('/events', 'EventController@index');
 Route::post('/uploads', 'FileUploader');
 Route::get('/file-details', 'GetFileDetails');
+
+Route::get('/uploads/{id}', 'UploadsController@show');
+
